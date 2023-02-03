2913e6b754ff072219dd648987e0dce8b86b29fa
ehye <yjw9610@hotmail.com>
2023-02-03 16:54:17 +0800

    Add SQLite

---
+++ Respawn.DatabaseTests/SqLiteTests.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.Data.Sqlite;
+using NPoco;
+using Shouldly;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Respawn.Graph;
+using Xunit.Abstractions;
+
+namespace Respawn.DatabaseTests
+{
+    public class SqLiteTests : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+        private readonly SqliteConnection _connection;
+        private readonly IDatabase _database;
+
+        public SqLiteTests(ITestOutputHelper output)
+        {
+            _output = output;
+
+            const string connString = @"Data Source=database.db";
+
+            _connection = new SqliteConnection(connString);
+            _connection.Open();
+
+            _database = new Database(_connection, DatabaseType.SQLite);
+        }
+
+        public class Foo
+        {
+            public int Value { get; set; }
+        }
+        public class Bar
+        {
+            public int Value { get; set; }
+        }
+        public class Bob
+        {
+            public int Value { get; set; }
+            public int FooValue { get; set; }
+        }
+
+        [SkipOnCI]
+        public async Task ShouldDeleteData()
+        {
+            _database.Execute("drop table if exists Foo");
+            _database.Execute("CREATE TABLE \"Foo\" (\"Value\" INTEGER);");
+
+            _database.InsertBulk(Enumerable.Range(0, 100).Select(i => new Foo { Value = i }));
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Foo").ShouldBe(100);
+
+            var checkpoint = await Respawner.CreateAsync(_connection, new RespawnerOptions
+            {
+                DbAdapter = DbAdapter.SQLite
+            });
+            await checkpoint.ResetAsync(_connection);
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Foo").ShouldBe(0);
+        }
+
+        [SkipOnCI]
+        public async Task ShouldIgnoreTables()
+        {
+            _database.Execute("drop table if exists Foo");
+            _database.Execute("drop table if exists Bar");
+            _database.Execute("create table Foo (Value INTEGER)");
+            _database.Execute("create table Bar (Value INTEGER)");
+            
+            await _database.InsertBulkAsync(Enumerable.Range(0, 100).Select(i => new Foo { Value = i }));
+            await _database.InsertBulkAsync(Enumerable.Range(0, 100).Select(i => new Bar { Value = i }));
+            
+            var checkPoint = await Respawner.CreateAsync(_connection, new RespawnerOptions()
+            {
+                DbAdapter = DbAdapter.SQLite,
+                TablesToIgnore = new Table[] { "Foo" }
+            });
+            await checkPoint.ResetAsync(_connection);
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Foo").ShouldBe(100);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Bar").ShouldBe(0);
+        }
+        
+        [SkipOnCI]
+        public async Task ShouldIncludeTables()
+        {
+            _database.Execute("drop table if exists Foo");
+            _database.Execute("drop table if exists Bar");
+            _database.Execute("create table Foo (Value INTEGER)");
+            _database.Execute("create table Bar (Value INTEGER)");
+
+            _database.InsertBulk(Enumerable.Range(0, 100).Select(i => new Foo { Value = i }));
+            _database.InsertBulk(Enumerable.Range(0, 100).Select(i => new Bar { Value = i }));
+
+            var checkpoint = await Respawner.CreateAsync(_connection, new RespawnerOptions
+            {
+                DbAdapter = DbAdapter.SQLite,
+                TablesToInclude = new Table[] { "Foo" }
+            });
+            await checkpoint.ResetAsync(_connection);
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Foo").ShouldBe(0);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Bar").ShouldBe(100);
+        }
+        
+        [SkipOnCI]
+        public async Task ShouldDeleteDataWithRelationships()
+        {
+            _database.Execute("drop table if exists Bar");
+            _database.Execute("drop table if exists Foo");
+            _database.Execute("drop table if exists Bob");
+
+            _database.Execute(@"
+CREATE TABLE Bob (
+  BobValue INTEGER NOT NULL, 
+  PRIMARY KEY (BobValue)
+)");
+
+            _database.Execute(@"
+CREATE TABLE Foo (
+  FooValue INTEGER NOT NULL,
+  BobValue INTEGER NOT NULL,
+  PRIMARY KEY (FooValue),
+  -- KEY IX_BobValue (BobValue),
+  CONSTRAINT FK_FOO_BOB FOREIGN KEY (BobValue) REFERENCES Bob (BobValue) ON DELETE NO ACTION ON UPDATE NO ACTION
+)");
+
+            _database.Execute(@"
+CREATE TABLE Bar (
+  BarValue INTEGER NOT NULL,
+  PRIMARY KEY (BarValue),
+  CONSTRAINT FK_BAR_FOO FOREIGN KEY (BarValue) REFERENCES Foo (FooValue) ON DELETE NO ACTION ON UPDATE NO ACTION
+)");
+
+            for (var i = 0; i < 100; i++)
+            {
+                _database.Execute($"INSERT INTO Bob VALUES ({i})");
+                _database.Execute($"INSERT INTO Foo VALUES ({i},{i})");
+                _database.Execute($"INSERT INTO Bar VALUES ({i})");
+            }
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Foo").ShouldBe(100);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Bar").ShouldBe(100);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Bob").ShouldBe(100);
+
+            var checkpoint = await Respawner.CreateAsync(_connection, new RespawnerOptions
+            {
+                DbAdapter = DbAdapter.SQLite
+            });
+            await checkpoint.ResetAsync(_connection);
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Foo").ShouldBe(0);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Bar").ShouldBe(0);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM Bob").ShouldBe(0);
+        }
+
+        [SkipOnCI]
+        public async Task ShouldHandleSelfRelationships()
+        {
+            _database.Execute("DROP table if exists Foo");
+            _database.Execute("CREATE TABLE \"Foo\" (\"id\"INTEGER,\"parent_id\"INTEGER,FOREIGN KEY(\"parent_id\") REFERENCES \"Foo\"(\"id\"),PRIMARY KEY(\"id\"));");
+            
+            _database.Execute("INSERT INTO Foo (id) VALUES (@0)", 1);
+            
+            for (int i = 1; i < 100; i++)
+            {
+                _database.Execute("INSERT INTO `foo` VALUES (@0, @1)", i + 1, i);
+            }
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM foo").ShouldBe(100);
+
+            var checkpoint = await Respawner.CreateAsync(_connection, new RespawnerOptions
+            {
+                DbAdapter = DbAdapter.SQLite
+            });
+            try
+            {
+                await checkpoint.ResetAsync(_connection);
+            }
+            catch
+            {
+                _output.WriteLine(checkpoint.DeleteSql ?? string.Empty);
+                throw;
+            }
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM foo").ShouldBe(0);
+        }
+
+        [SkipOnCI]
+        public async Task ShouldHandleCircularRelationships()
+        {
+            _database.Execute("DROP TABLE if EXISTS parent");
+            _database.Execute("DROP TABLE if EXISTS child");
+            _database.Execute("CREATE TABLE parent ( id INTEGER, child_id INTEGER NULL, FOREIGN KEY(child_id) REFERENCES child(parent_id), PRIMARY KEY(id) );");
+            _database.Execute("CREATE TABLE child ( id INTEGER, parent_id INTEGER NULL, FOREIGN KEY(parent_id) REFERENCES parent(child_id), PRIMARY KEY(id) );");
+
+            _database.Execute("PRAGMA foreign_keys = OFF;");
+
+            for (int i = 0; i < 100; i++)
+            {
+                _database.Execute($"INSERT INTO parent VALUES ({i}, null)");
+                _database.Execute($"INSERT INTO child VALUES ({i}, null)");
+            }
+
+            _database.Execute("update parent set child_id = 0");
+            _database.Execute("update child set parent_id = 1");
+            
+            _database.Execute("PRAGMA foreign_keys = OFF;");
+            
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM parent").ShouldBe(100);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM child").ShouldBe(100);
+
+            var checkpoint = await Respawner.CreateAsync(_connection, new RespawnerOptions
+            {
+                DbAdapter = DbAdapter.SQLite
+            });
+            await checkpoint.ResetAsync(_connection);
+
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM parent").ShouldBe(0);
+            _database.ExecuteScalar<int>("SELECT COUNT(1) FROM child").ShouldBe(0);
+        }
+        
+        public void Dispose()
+        {
+            _connection.Close();
+            _connection.Dispose();
+        }
+    }
+}
---
+++ Respawn/SQLiteAdapter.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Respawn.Graph;
+
+namespace Respawn;
+
+public class SQLiteAdapter : IDbAdapter
+{
+    private const char QuoteCharacter = '`';
+
+    public string BuildTableCommandText(RespawnerOptions options)
+    {
+        string commandText = "SELECT t.name, t.tbl_name FROM sqlite_schema t WHERE type = 'table'";
+
+        if (options.TablesToIgnore.Any())
+        {
+            var tablesToIgnoreGroups = options.TablesToIgnore
+                .GroupBy(
+                    t => t.Schema != null,
+                    t => t,
+                    (hasSchema, tables) => new { HasSchema = hasSchema, Tables = tables })
+                .ToList();
+            foreach (var tableGroup in tablesToIgnoreGroups)
+            {
+                if (tableGroup.HasSchema)
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Schema}.{table.Name}'"));
+
+                    commandText += " AND t.name + '.' + t.tbl_name NOT IN (" + args + ")";
+                }
+                else
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Name}'"));
+
+                    commandText += " AND t.tbl_name NOT IN (" + args + ")";
+                }
+            }
+        }
+        if (options.TablesToInclude.Any())
+        {
+            var tablesToIncludeGroups = options.TablesToInclude
+                .GroupBy(
+                    t => t.Schema != null,
+                    t => t,
+                    (hasSchema, tables) => new { HasSchema = hasSchema, Tables = tables })
+                .ToList();
+            foreach (var tableGroup in tablesToIncludeGroups)
+            {
+                if (tableGroup.HasSchema)
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Schema}.{table.Name}'"));
+
+                    commandText += " AND t.name + '.' + t.tbl_name IN (" + args + ")";
+                }
+                else
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Name}'"));
+
+                    commandText += " AND t.tbl_name IN (" + args + ")";
+                }
+            }
+        }
+        if (options.SchemasToExclude.Any())
+        {
+            var args = string.Join(",", options.SchemasToExclude.Select(t => $"'{t}'"));
+
+            commandText += " AND t.name NOT IN (" + args + ")";
+        }
+        else if (options.SchemasToInclude.Any())
+        {
+            var args = string.Join(",", options.SchemasToInclude.Select(t => $"'{t}'"));
+
+            commandText += " AND t.name IN (" + args + ")";
+        }
+
+        return commandText;
+    }
+
+    public string BuildTemporalTableCommandText(RespawnerOptions options) => throw new System.NotImplementedException();
+
+    public string BuildRelationshipCommandText(RespawnerOptions options)
+    {
+        var commandText = """
+SELECT 
+	m.name sch_name,
+	m.tbl_name,
+	p."table" schema,
+	p."table",
+	replace( substr(m.sql, 1, instr(m.sql, '" FOREIGN KEY ')-1), substr(m.sql, 1, instr(m.sql, 'FK_')-1),'') constraint_name
+FROM
+	sqlite_master m
+JOIN pragma_foreign_key_list(m.name) p ON m.name != p."table"
+""";
+
+        var whereText = new List<string>();
+
+        if (options.TablesToIgnore.Any())
+        {
+            var tablesToIgnoreGroups = options.TablesToIgnore
+                .GroupBy(
+                    t => t.Schema != null,
+                    t => t,
+                    (hasSchema, tables) => new
+                    {
+                        HasSchema = hasSchema,
+                        Tables = tables
+                    })
+                .ToList();
+            foreach (var tableGroup in tablesToIgnoreGroups)
+            {
+                if (tableGroup.HasSchema)
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Schema}.{table.Name}'"));
+
+                    whereText.Add("sch_name + '.' + tbl_name NOT IN (" + args + ")");
+                }
+                else
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Name}'"));
+
+                    whereText.Add("tbl_name NOT IN (" + args + ")");
+                }
+            }
+        }
+        if (options.TablesToInclude.Any())
+        {
+            var tablesToIncludeGroups = options.TablesToInclude
+                .GroupBy(
+                    t => t.Schema != null,
+                    t => t,
+                    (hasSchema, tables) => new
+                    {
+                        HasSchema = hasSchema,
+                        Tables = tables
+                    })
+                .ToList();
+            foreach (var tableGroup in tablesToIncludeGroups)
+            {
+                if (tableGroup.HasSchema)
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Schema}.{table.Name}'"));
+
+                    whereText.Add("name + '.' + TABLE_NAME IN (" + args + ")");
+                }
+                else
+                {
+                    var args = string.Join(",", tableGroup.Tables.Select(table => $"'{table.Name}'"));
+
+                    whereText.Add("tbl_name IN (" + args + ")");
+                }
+            }
+        }
+        if (options.SchemasToExclude.Any())
+        {
+            var args = string.Join(",", options.SchemasToExclude.Select(t => $"'{t}'"));
+            whereText.Add("sch_name NOT IN (" + args + ")");
+        }
+        else if (options.SchemasToInclude.Any())
+        {
+            var args = string.Join(",", options.SchemasToInclude.Select(t => $"'{t}'"));
+            whereText.Add("sch_name IN (" + args + ")");
+        }
+
+        if (whereText.Any())
+            commandText += $" WHERE {string.Join(" AND ", whereText.ToArray())}";
+        return commandText;
+    }
+
+    public string BuildDeleteCommandText(GraphBuilder graph)
+    {
+        var builder = new StringBuilder();
+
+        builder.AppendLine("PRAGMA foreign_keys = OFF;");
+        foreach (var table in graph.ToDelete)
+        {
+            builder.AppendLine($"DELETE FROM {table.Name};");
+        }
+        builder.AppendLine("PRAGMA foreign_keys = ON;");
+
+        return builder.ToString();
+    }
+
+    public string BuildReseedSql(IEnumerable<Table> tablesToDelete)
+    {
+        var builder = new StringBuilder();
+        foreach (var table in tablesToDelete)
+        {
+            builder.AppendLine($"PRAGMA automatic_index = ON;");
+        }
+
+        return builder.ToString();
+    }
+
+    public string BuildTurnOffSystemVersioningCommandText(IEnumerable<TemporalTable> tablesToTurnOffSystemVersioning) => throw new System.NotImplementedException();
+
+    public string BuildTurnOnSystemVersioningCommandText(IEnumerable<TemporalTable> tablesToTurnOnSystemVersioning) => throw new System.NotImplementedException();
+
+    public Task<bool> CheckSupportsTemporalTables(DbConnection connection)
+    {
+        return Task.FromResult(false);
+    }
+}
